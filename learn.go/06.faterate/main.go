55b72ecd8874a4f12e7555f87449c028929e85ba
Churzhi <chruzhi@qq.com>
2021-12-05 15:03:55 +0800

    完成BMI计算，输入三位用户数据，并输出三位用户的BMI计算

--- learn.go/06.faterate/main.go
+++ learn.go/06.faterate/main.go
@@ -16,134 +16,138 @@ func main() {
 		}
 
 		var person Person
-		users := [3]Person{person}
-		// 用户输入
-		fmt.Print("请输入用户数据：")
+		const numbers = 3
+		users := [numbers]Person{person}
 
-		fmt.Print("姓名：")
-		fmt.Scanln(&person.name)
+		fmt.Print("请输入一组用户数据（3个用户一组）：\n")
+		for i := 0; i < numbers; i++ {
+			// 用户输入
+			fmt.Printf("请输入第%d个用户数据\n", i+1)
+			fmt.Print("姓名：")
+			fmt.Scanln(&person.name)
 
-		fmt.Print("体重（kg）：")
-		fmt.Scanln(&person.weight)
+			fmt.Print("体重（kg）：")
+			fmt.Scanln(&person.weight)
 
-		fmt.Print("身高（m）：")
-		fmt.Scanln(&person.tall)
+			fmt.Print("身高（cm）：")
+			fmt.Scanln(&person.tall)
 
-		fmt.Print("年龄：")
-		fmt.Scanln(&person.age)
+			fmt.Print("年龄：")
+			fmt.Scanln(&person.age)
 
-		fmt.Print("性别(男/女)：")
-		fmt.Scanln(&person.sex)
-		for {
-			if person.sex != "男" && person.sex != "女" {
-				fmt.Println("输入错误，请重新输入！")
-				fmt.Println("性别(男/女)：")
-				fmt.Scanln(&person.sex)
-			} else {
-				break
+			fmt.Print("性别(男/女)：")
+			fmt.Scanln(&person.sex)
+			for {
+				if person.sex != "男" && person.sex != "女" {
+					fmt.Println("输入错误，请重新输入！")
+					fmt.Println("性别(男/女)：")
+					fmt.Scanln(&person.sex)
+				} else {
+					break
+				}
 			}
+			if person.sex == "男" {
+				person.sexWeight = 1
+			} else {
+				person.sexWeight = 0
+			}
+			users[i] = person
 		}
-		users[0] = person
-		fmt.Println("person", users)
 
-		if person.sex == "男" {
-			person.sexWeight = 1
-		} else {
-			person.sexWeight = 0
-		}
-		fmt.Println(person.name, person.weight, person.tall, person.age, person.sex)
-		var bmi = person.weight / (person.tall * person.tall)
-		var fatRate = (1.2*bmi + 0.23*float64(person.age) - 5.4 - 10.8*float64(person.sexWeight)) / 100
-		fmt.Printf("体脂率是：%f", fatRate)
+		for i, personVal := range users {
 
-		if person.sex == "男" {
-			// 判断男性BMI
-			if person.age >= 18 && person.age <= 39 {
-				if fatRate <= 0.1 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.1 && fatRate <= 0.16 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.16 && fatRate <= 0.21 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.21 && fatRate <= 0.26 {
-					fmt.Println("目前是：肥胖，少吃点")
+			var bmi = personVal.weight / ((personVal.tall * personVal.tall) / 10000)
+			var fatRate = (1.2*bmi + 0.23*float64(personVal.age) - 5.4 - 10.8*float64(personVal.sexWeight)) / 100
+			var BMIData string
+			if personVal.sex == "男" {
+				// 判断男性BMI
+				if personVal.age >= 18 && personVal.age <= 39 {
+					if fatRate <= 0.1 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.1 && fatRate <= 0.16 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.16 && fatRate <= 0.21 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.21 && fatRate <= 0.26 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：非常肥胖!"
+					}
+				} else if personVal.age >= 40 && personVal.age <= 59 {
+					if fatRate <= 0.11 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.11 && fatRate <= 0.17 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.17 && fatRate <= 0.22 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.22 && fatRate <= 0.27 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：非常肥胖!"
+					}
+				} else if personVal.age >= 60 {
+					if fatRate <= 0.13 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.13 && fatRate <= 0.19 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.19 && fatRate <= 0.24 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.24 && fatRate <= 0.29 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：非常肥胖!"
+					}
 				} else {
-					fmt.Println("目前是：非常肥胖!")
+					BMIData = "未成年"
 				}
-			} else if person.age >= 40 && person.age <= 59 {
-				if fatRate <= 0.11 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.11 && fatRate <= 0.17 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.17 && fatRate <= 0.22 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.22 && fatRate <= 0.27 {
-					fmt.Println("目前是：肥胖，少吃点")
+			} else if personVal.sex == "女" {
+				// 判断女性BMI
+				if personVal.age >= 18 && personVal.age <= 39 {
+					if fatRate <= 0.2 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.20 && fatRate <= 0.27 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.27 && fatRate <= 0.34 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.34 && fatRate <= 0.39 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：非常肥胖!"
+					}
+				} else if personVal.age >= 40 && personVal.age <= 59 {
+					if fatRate <= 0.21 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.21 && fatRate <= 0.28 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.28 && fatRate <= 0.35 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.35 && fatRate <= 0.40 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：非常肥胖!"
+					}
+				} else if personVal.age >= 60 {
+					if fatRate <= 0.22 {
+						BMIData = "目前是：偏瘦"
+					} else if fatRate > 0.22 && fatRate <= 0.29 {
+						BMIData = "目前是：标准"
+					} else if fatRate > 0.29 && fatRate <= 0.36 {
+						BMIData = "目前是：偏重"
+					} else if fatRate > 0.36 && fatRate <= 0.41 {
+						BMIData = "目前是：肥胖，少吃点"
+					} else {
+						BMIData = "目前是：肥胖，少吃点"
+					}
 				} else {
-					fmt.Println("目前是：非常肥胖!")
+					BMIData = "未成年"
 				}
-			} else if person.age >= 60 {
-				if fatRate <= 0.13 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.13 && fatRate <= 0.19 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.19 && fatRate <= 0.24 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.24 && fatRate <= 0.29 {
-					fmt.Println("目前是：肥胖，少吃点")
-				} else {
-					fmt.Println("目前是：非常肥胖!")
-				}
-			} else {
-				fmt.Println("未成年")
 			}
-		} else if person.sex == "女" {
-			// 判断女性BMI
-			if person.age >= 18 && person.age <= 39 {
-				if fatRate <= 0.2 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.20 && fatRate <= 0.27 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.27 && fatRate <= 0.34 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.34 && fatRate <= 0.39 {
-					fmt.Println("目前是：肥胖，少吃点")
-				} else {
-					fmt.Println("目前是：非常肥胖!")
-				}
-			} else if person.age >= 40 && person.age <= 59 {
-				//todo
-				if fatRate <= 0.21 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.21 && fatRate <= 0.28 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.28 && fatRate <= 0.35 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.35 && fatRate <= 0.40 {
-					fmt.Println("目前是：肥胖，少吃点")
-				} else {
-					fmt.Println("目前是：非常肥胖!")
-				}
-			} else if person.age >= 60 {
-				//todo
-				if fatRate <= 0.22 {
-					fmt.Println("目前是：偏瘦")
-				} else if fatRate > 0.22 && fatRate <= 0.29 {
-					fmt.Println("目前是：标准")
-				} else if fatRate > 0.29 && fatRate <= 0.36 {
-					fmt.Println("目前是：偏重")
-				} else if fatRate > 0.36 && fatRate <= 0.41 {
-					fmt.Println("目前是：肥胖，少吃点")
-				} else {
-					fmt.Println("目前是：非常肥胖!")
-				}
-			} else {
-				fmt.Println("未成年")
-			}
+			fmt.Printf("第%d位用户 %s 的体脂率是：%f，%s\n", i+1, personVal.name, fatRate, BMIData)
+
 		}
 
 		var whetherContinue string
-		fmt.Print("是否录入下一个(y/n):")
+		fmt.Print("是否录入下一组数据n(y/n):")
 		fmt.Scanln(&whetherContinue)
 		if whetherContinue != "y" {
 			break
