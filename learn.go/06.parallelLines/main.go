38fc12b9966ad6af1fea64843b32dd12019f59cf
Churzhi <chruzhi@qq.com>
2021-12-07 23:40:41 +0800

    判断两条直线是否平行

--- learn.go/06.parallelLines/main.go
+++ learn.go/06.parallelLines/main.go
@@ -16,30 +16,69 @@ import (
 */
 func main() {
 
-	type coordinate struct {
-		x float64
-		y float64
+	type Coordinate struct {
+		x1        float64
+		y1        float64
+		x2        float64
+		y2        float64
+		gradient  float64
+		intercept float64
 	}
-	var (
-		lineOneX1 float64
-		lineOneY1 float64
-		lineOneX2 float64
-		lineOneY2 float64
-		lineTwoX1 float64
-		lineTwoY1 float64
-		lineTwoX2 float64
-		lineTwoY2 float64
 
-		//斜率
-		gradient float64
-	)
+	var coordinate Coordinate
+	lineCoordinates := [2]Coordinate{coordinate}
 
-	fmt.Println("请输入直线1的两个（X，Y）坐标：")
-	fmt.Scanln(&lineOneX1, &lineOneY1, &lineOneX2, &lineOneY2)
+	for i := 0; i < 2; i++ {
+		fmt.Println("请输入直线", i+1, "的两个（X，Y）坐标：")
+		coordinate.x1, coordinate.y1, coordinate.x2, coordinate.y2 = getLineCoordinate()
+		coordinate.gradient, coordinate.intercept = calcGradientAndIntercept(coordinate.x1, coordinate.y1, coordinate.x2, coordinate.y2)
+		lineCoordinates[i] = coordinate
+	}
+	fmt.Println("coordinate:", lineCoordinates)
 
-	fmt.Println("请输入直线2的（X，Y）坐标：")
-	fmt.Scanln(&lineTwoX1, &lineTwoY1, &lineTwoX2, &lineTwoY2)
+	for i := 0; i < len(lineCoordinates); i++ {
+		if lineCoordinates[i].gradient == lineCoordinates[i+1].gradient && lineCoordinates[i].intercept != lineCoordinates[i+1].intercept {
+			fmt.Println("直线", i+1, "和直线", i+2, "平行")
+		} else if lineCoordinates[i].gradient == lineCoordinates[i+1].gradient && lineCoordinates[i].intercept == lineCoordinates[i+1].intercept {
+			fmt.Println("直线", i+1, "和直线", i+2, "重合")
 
+		} else if lineCoordinates[i].gradient*lineCoordinates[i+1].gradient == -1 {
+			fmt.Println("直线", i+1, "和直线", i+2, "垂直")
+		} else {
+			fmt.Println("直线", i+1, "和直线", i+2, "不平行")
+
+		}
+	}
+}
+
+func getLineCoordinate() (x1 float64, y1 float64, x2 float64, y2 float64) {
+
+	for {
+		fmt.Scanln(&x1, &y1, &x2, &y2)
+		if x1 == x2 && y1 == y2 {
+			fmt.Println("直线两点不能相同哦，请重新输入：")
+		}
+		break
+	}
+	return x1, y1, x2, y2
+}
+
+func calcGradientAndIntercept(x1 float64, y1 float64, x2 float64, y2 float64) (gradient float64, intercept float64) {
 	// 点斜式：y-y0=k（x-x0），斜率就是k
-	gradient =
+	// y = kx +b
+
+	/*若直线L1:A1x+B1y+C1 =0与直线 L2:A2x+B2y+C2=0
+
+	1. 当A1B2-A2B1≠0时, 相交
+
+	2.A1/A2=B1/B2≠C1/C2, 平行
+
+	3.A1/A2=B1/B2=C1/C2, 重合
+
+	4.A1A2+B1B2=0, 垂直
+
+	*/
+	gradient = (y2 - y1) / (x2 - x1)
+	intercept = y1 - (gradient * x1)
+	return gradient, intercept
 }
