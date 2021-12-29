5df68a9926cb17b8131b4a2b31d76f1981e774e0
Jim_zhou <churzhi@qq.com>
2021-12-30 00:36:00 +0800

    fix 作业1 calc_bmi.go 错误

--- learn.go/chapter03/00.faterate.refactor/calc/calc_bmi.go
+++ learn.go/chapter03/00.faterate.refactor/calc/calc_bmi.go
@@ -1,7 +1,9 @@
 package calc
 
-func CalcBMI(weightKG, tallM float64) (bmi float64) {
+import gobmi "github.com/armstrongli/go-bmi"
 
-	bmi = weightKG / (tallM * tallM)
+func CalcBMI(weightKG, tallM float64) (bmi float64) {
+	bmi, _ = gobmi.BMI(weightKG, tallM)
+
 	return
 }
