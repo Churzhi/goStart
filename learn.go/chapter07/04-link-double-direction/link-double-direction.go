da3bafa051e0cba5330fe10e773fbfa273f4c1b2
Jim_zhou <churzhi@qq.com>
2022-03-20 16:05:51 +0800

    deleteNode(): - 只有一个节点时，返回一个空节点

--- learn.go/chapter07/04-link-double-direction/link-double-direction.go
+++ learn.go/chapter07/04-link-double-direction/link-double-direction.go
@@ -10,8 +10,8 @@ type LinkNode struct {
 
 func buildDLink() *LinkNode {
 	n1 := &LinkNode{data: 1}
-	n2 := &LinkNode{data: 1}
-	n3 := &LinkNode{data: 1}
+	n2 := &LinkNode{data: 5}
+	n3 := &LinkNode{data: 10}
 
 	n1.next = n2
 	n2.previous = n1
@@ -38,12 +38,15 @@ func insertNode(root *LinkNode, newNode *LinkNode) *LinkNode {
 	}
 
 	for {
+		// 尾部插入
 		if tmpNode.next == nil {
 			// 已经到头，追加节点即可
 			tmpNode.next = newNode
 			newNode.previous = tmpNode
 			return root
+
 		} else {
+			// 中间插入
 			if tmpNode.next.data >= newNode.data {
 				newNode.previous = tmpNode
 				newNode.next = tmpNode.next
@@ -60,39 +63,48 @@ func insertNode(root *LinkNode, newNode *LinkNode) *LinkNode {
 func deleteNode(root *LinkNode, v int) *LinkNode {
 	if root == nil {
 		return nil
-
 	}
+	tmpNode := root
+
 	// 如果需要删除的数据在头节点
 	if root.data == v {
+		// 要删除的数据在第一个节点
 		leftHand := root
 		root = root.next
 
 		leftHand.next = nil
 		root.previous = nil
 
-		// TODO 需要解决只有一个节点的情况
+		// 只有一个节点的情况，返回一个空节点
+		if root.next == nil {
+			nullNode := &LinkNode{}
+			return nullNode
+		}
 		return root
-
 	}
 
-	tmpNode := root
 	for {
 		if tmpNode.next == nil {
-			// 走到链表尾部，仍然没有找到要删除的数据，直接返回原 root
+			// 如果走到链表尾部，还没找到要删除的数据，直接返回原 root
 			return root
+
 		} else {
 			if tmpNode.next.data == v {
-				// 找到节点 开始删除
-				// TODO
+				// 找到节点，开始删除，删除完成后返回原 root
 				rightHand := tmpNode.next
 				tmpNode.next = rightHand.next
-				rightHand.next.previous = tmpNode
+				rightHand.next.previous = tmpNode // tmpNode.next.previous 均可
 
-				// 清理掉右手上的
+				// 清理掉右手上的 link，保证 GC 正常回收
+				rightHand.next = nil
+				rightHand.previous = nil
 				return root
 			}
 		}
+		tmpNode = tmpNode.next
+
 	}
+
 }
 
 func rangeLink(root *LinkNode) {
@@ -112,20 +124,23 @@ func rangeLink(root *LinkNode) {
 		if tmpNode.previous == nil {
 			break
 		}
-
+		tmpNode = tmpNode.previous
 	}
 }
 
 func main() {
 
 	root := buildDLink()
-	// 需要及时变更 root 的位置
-	root = insertNode(root, &LinkNode{data: 3})
-	root = insertNode(root, &LinkNode{data: 7})
-	root = insertNode(root, &LinkNode{data: 11})
-	root = insertNode(root, &LinkNode{data: 0})
+	// 需要及时变更 root 的位置，因为如果在头节点前插入时，不更新root的话遍历出来的还是原来的头节点
+	//root = insertNode(root, &LinkNode{data: 3})
+	//root = insertNode(root, &LinkNode{data: 7})
+	//root = insertNode(root, &LinkNode{data: 11})
+	//root = insertNode(root, &LinkNode{data: 0})
 	rangeLink(root)
 
 	fmt.Println("删除节点")
-	root = deleteNode(root, 7)
+	root = deleteNode(root, 1)
+	root = deleteNode(root, 5)
+	root = deleteNode(root, 10)
+	rangeLink(root)
 }
