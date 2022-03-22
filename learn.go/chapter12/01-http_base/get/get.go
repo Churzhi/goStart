21e188705b5fc5d177a0a1c5bcce5ae22560d7c2
Jim_zhou <churzhi@qq.com>
2022-03-22 23:00:58 +0800

    http get 样例

---
+++ learn.go/chapter12/01-http_base/get/get.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		log.Fatal("http请求失败：", err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("无法读取返回内容：", err)
+	}
+	fmt.Println(string(data))
+
+}
