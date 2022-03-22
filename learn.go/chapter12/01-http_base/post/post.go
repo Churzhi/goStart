bade367c9f6a0c98191754d8de5ffb7de9eb1bb4
Jim_zhou <churzhi@qq.com>
2022-03-22 23:01:07 +0800

    http post 样例

--- learn.go/chapter12/01-http_base/post/post.go
+++ learn.go/chapter12/01-http_base/post/post.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
 func main() {
-	$END$
+	p := strings.NewReader("foooo")
+	resp, err := http.Post("https://www.baidu.com", "*/*", p)
+	if err != nil {
+		log.Fatal("无法发送请求：", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal("Body关闭失败：", err)
+		}
+	}(resp.Body)
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("无法读取返回内容：", err)
+	}
+	fmt.Println(data)
 }
